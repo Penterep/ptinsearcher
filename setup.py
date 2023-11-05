a453dab106b750b2d2f115592c3702999bd92cd7
Daniel <dan@soom.cz>
2023-11-05 18:54:26 -0500

    Add missing dependencies

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
     ],
     python_requires = '>=3.6',
-    install_requires=["ptlibs>=1,<2", "requests", "bs4", "lxml", "tldextract>=5.0.0", "pyexiftool"],
+    install_requires=["ptlibs>=1,<2", "requests", "bs4", "lxml", "tldextract>=5.0.0", "pyexiftool", "validators", "python-magic"],
     entry_points = {'console_scripts': ['ptinsearcher = ptinsearcher.ptinsearcher:main']},
     include_package_data= True,
     long_description=long_description,
